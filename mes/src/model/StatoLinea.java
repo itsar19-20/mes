cd82573c94f5832a919fcdf6660e20eda0815fc4
giada-cpu <****@****>
2020-01-05 16:09:00 +0100

    Add files via upload

--- mes/src/model/StatoLinea.java
+++ mes/src/model/StatoLinea.java
@@ -1,5 +1,40 @@
-package model;
+package lavoroMes;
+
+import java.util.Date;
 
 public class StatoLinea {
 
+	private Date timestamp;
+	private LineaDiProduzione linea;
+	private StatiLinea statoLinea;
+	
+	/* 
+	 * getters
+	 */
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public LineaDiProduzione getLinea() {
+		return linea;
+	}
+	public StatiLinea getStatoLinea() {
+		return statoLinea;
+	}
+	
+	/*
+	 * setters
+	 */
+	
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public void setLinea(LineaDiProduzione linea) {
+		this.linea = linea;
+	}
+	
+	public void setStatoLinea(StatiLinea statoLinea) {
+		this.statoLinea = statoLinea;
+	}
 }
