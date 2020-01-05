377b80dd459d6fc7a8837ce5fd8e15c824254cf5
Dany2697 <****@****>
2020-01-05 16:36:54 +0100

    Add files via upload

---
+++ mes/src/model/Messaggio.java
@@ -0,0 +1,23 @@
+package classiMes;
+
+public class Messaggio {
+
+	private String testo;
+
+	/*
+	 * getter
+	 */
+	
+	public String getTesto() {
+		return testo;
+	}
+
+	/*
+	 * setter
+	 */
+	
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+	
+}
