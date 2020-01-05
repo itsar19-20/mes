9ba27221bda35c7fb7699210f0081b0b9d7022ff
giada-cpu <57758594+giada-cpu@users.noreply.github.com>
2020-01-05 16:42:49 +0100

    Add files via upload

--- mes/src/model/Utente.java
+++ mes/src/model/Utente.java
@@ -1,7 +1,7 @@
 package mes;
 
 public class Utente {
-
+	
 	private String ruolo;
 	private int id;
 	private String nome;
@@ -12,4 +12,37 @@ public class Utente {
 		return null;
 		
 	}
+
+	public String getRuolo() {
+		return ruolo;
+	}
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
