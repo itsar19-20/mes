ffabd058976ee9c1941e0ab9aa0c922aa5f64502
giada-cpu <57758594+giada-cpu@users.noreply.github.com>
2020-01-05 16:38:12 +0100

    Add files via upload

---
+++ mes/src/model/Utente.java
@@ -0,0 +1,15 @@
+package mes;
+
+public class Utente {
+
+	private String ruolo;
+	private int id;
+	private String nome;
+	private String password;
+	
+	public Boolean verifica() {
+		/*da implementare*/
+		return null;
+		
+	}
+}
