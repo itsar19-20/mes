55ca75883468ab80cd74103e819c38660f884c0f
Dany2697 <d.tozzi@itsrizzoli.it>
2020-02-03 16:51:15 +0100

    riviste pagine web

--- web/Login/login.js
+++ web/Login/login.js
@@ -1,8 +1,24 @@
-function myFunction() {
-    var x = document.getElementById("pwd");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
+$(() => {
+    localStorage.removeItem('user');
+    // una funzione () senza parametri
+    $('#btnLogin').click(() => {
+        $.ajax({
+            url: '/login',
+            method: 'post',
+            data: { 
+                username: $('#inputUsername').val(), 
+                password: $('#inputPassword').val()
+            }
+        })
+
+              // una funzione che ha parametro UTENTE 
+        .done((utente) => {
+            if (utente) {
+                localStorage.setItem('user', JSON.stringify(utente));
+                location.href = './home.html';
+            } else {
+                $('#msgFail').show();
+            }
+        })
+    });
+});
