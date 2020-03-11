11e06852b3d8a451ac0ea343450dc70110438a89
Daniele <d.tozzi@itsrizzoli.it>
2020-03-11 17:35:43 +0100

    aggiunta schermata home e registrazione

---
+++ android/schermata home e registrazione/app/src/main/java/com/example/ecommerce2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button user,carrello;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user = findViewById(R.id.user);
+        carrello = findViewById(R.id.carrello);
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
