2e7b5e1b8aa9170c7000febdb5b9e9eeed584adf
Daniele <****@****>
2020-03-17 16:30:51 +0100

    ...

--- android/ECommerce2/app/src/main/java/com/example/ecommerce2/LoginActivity.java
+++ android/ECommerce2/app/src/main/java/com/example/ecommerce2/LoginActivity.java
@@ -3,8 +3,11 @@ package com.example.ecommerce2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,6 +15,11 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 Button registrati,accedi;
 EditText email,password;
+DatabaseHelper databaseHelper;
+DatabaseAdapter databaseAdapter;
+Cursor utente;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,10 @@ EditText email,password;
         accedi = findViewById(R.id.accedi);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
+        databaseAdapter = new DatabaseAdapter(this);
+        databaseAdapter.open();
+        databaseHelper = new DatabaseHelper(this);
 
         registrati.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,10 +45,22 @@ EditText email,password;
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this,MainActivity.class);
-
                 if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
                     Toast.makeText(LoginActivity.this,"Attenzione, compila tutti i campi prima di procedere",Toast.LENGTH_SHORT).show();
+
                 } else {
+                    String passwordLog = password.getText().toString();
+                    try{
+                        utente = databaseAdapter.cercaConEmail(email.getText().toString());
+                        utente.moveToFirst();
+                        if (utente.getString(1).contentEquals(passwordLog)){
+                            Toast.makeText(LoginActivity.this,"Login effettuato con successo!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this,"Password errata",Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (Exception e){
+
+                    }
                     startActivity(i);
                 }
             }
