ced141e57c4b83a48d0bbdfde49a545e887e24f6
giuliocigno <57756052+giuliocigno@users.noreply.github.com>
2020-04-16 16:04:56 +0200

    scada service...

---
+++ scada/src/test/java/mes/scada/ScadaApplicationTests.java
@@ -0,0 +1,13 @@
+package mes.scada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
